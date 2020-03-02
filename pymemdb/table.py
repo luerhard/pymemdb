d86fb4ea9a7d843a249f23ac36698f00cede672b
Lukas Erhard <luerhard@googlemail.com>
2020-03-02 17:14:46 +0100

    Add some docstrings to Table

--- pymemdb/table.py
+++ pymemdb/table.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from collections.abc import Iterable
-from typing import Optional, Generator, Union, Hashable, List, Dict, Set
+from typing import Optional, Generator, Union, Hashable, List, Dict
 from pymemdb import Column, ColumnDoesNotExist
 
 import sys
@@ -11,13 +11,15 @@ if version.major < 3:  # pragma: no cover
 if version.major >= 3 and version.minor < 8:
     ORDER_TYPE = Union[str, bool]
 else:  # pragma: no cover
-    from typing import Literal
-    ORDER_TYPE = Literal["ascending", "descending", False]
+    from typing import Literal  # type: ignore
+    ORDER_TYPE = Literal["ascending", "descending", False]  # type:ignore
 
 ROW_GEN = Generator[dict, None, None]
 
 
 class Table:
+    """Object that represents a Table in the Database.
+       Can also used standalone"""
 
     def __init__(self, name: Optional[str] = None,
                  primary_id: str = "id") -> None:
@@ -29,6 +31,23 @@ class Table:
         self.create_column(name=self.idx_name, unique=True)
 
     def all(self, ordered: ORDER_TYPE = False) -> ROW_GEN:
+        """returns a generator of all rows of the table.
+
+        Keyword Arguments:
+            ordered {["ascending", "descending", False]} -- Can be set to
+                ascending or descending to sort by the primary id column
+                (default: {False})
+        Raises:
+            ValueError: [if 'ordered' is not in {"ascending', 'descending',
+                                                 False}]
+
+        Returns:
+            Generator -- [A Generator over all the rows in the table]
+
+        Yields:
+            dict -- [Dictionary that contains all elements of a row in the
+                     table]
+        """
         if ordered is False:
             for i in self.keys:
                 yield self._get_row(i)
@@ -44,16 +63,48 @@ class Table:
 
     def create_column(self, name: str, default: Hashable = None,
                       unique: bool = False) -> None:
+        """Create a Column in the table.
+
+        Arguments:
+            name {str} -- Name of the column
+
+        Keyword Arguments:
+            default {Hashable} -- Default value for the column
+                                    (default: {None})
+            unique {bool} -- If values are enforced to be unique for this
+                             column. If True, trying to insert a value more
+                             than once will raise UniqueConstraintError
+                             (default: {False})
+        """
         self._columns[name] = Column(default=default, unique=unique)
 
     @property
     def columns(self) -> List[str]:
+        """Returns a list of all column names of the table.
+
+        Returns:
+            List[str] -- [List of column names]
+
+        """
         return list(self._columns)
 
     def drop(self) -> None:
+        """Delete table."""
         del self
 
     def insert(self, row: Dict) -> int:
+        """Inserts a row in the table. If a column is not present,
+           it will be created with default value None
+
+        Arguments:
+            row {Dict} -- dictionary that represents a row in the table.
+
+        Raises:
+            UniqueConstraintError: [if constraint of a column is violated]
+
+        Returns:
+            int -- [primary key for the row inserted]
+        """
         if self.idx_name in row:
             idx = row[self.idx_name]
         else:
@@ -68,6 +119,22 @@ class Table:
         return idx
 
     def insert_ignore(self, row: Dict, keys: List[str]) -> Optional[int]:
+        """Inserts rows into the table. If another row is already present
+           where all the values are identical for the fields in 'keys', the
+           insert will be skipped
+
+        Arguments:
+            row {Dict} -- [row to be inserted]
+            keys {List[str]} -- [List of columns to check for ignore]
+
+        Raises:
+            UniqueConstraintError: [if constraint of a column is violated]
+
+        Returns:
+            Optional[int] -- [primary key for the row inserted or None if
+                              the insert was skipped]
+
+        """    
         results = self.find(**{key: row[key] for key in keys})
         try:
             next(results)
@@ -77,6 +144,22 @@ class Table:
 
     def find(self, ignore_errors: bool = False,
              **kwargs) -> Generator[dict, None, None]:
+        """finds rows in the table.
+
+
+        Keyword Arguments:
+            **kwargs -- keyword is the column name and value represents the
+                        value to search.
+                        if value is in iterable, it matches a
+                        SELECT * WHERE keyword IN val
+                        search.
+            ignore_errors {bool} -- if True, it raises an error if a column
+                                    does not exist in the table
+                                    (default: {False})
+        
+        Returns:
+            Generator[dict] -- [Generator over all rows that match the search]
+        """
         results = self._find_rows(ignore_errors=ignore_errors, **kwargs)
         if not results:
             return None
