bc3aaaf7f48bbe118fe90117750db2699bcf875f
Lukas Erhard <luerhard@googlemail.com>
2020-05-12 11:13:48 +0200

    Bump version: 1.3.0 → 1.4.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pymemdb",
-    version="1.3.0",
+    version="1.4.0",
     author_email="luerhard@googlemail.com",
     description=("A very fast in-memory database with export to sqlite written purely in python"),
     long_description=read("README.md"),
